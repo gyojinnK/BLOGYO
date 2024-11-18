5520f1636e9df97a8ed0ec2ccdd38182df391f1e
gyojinnk <ksmug1998@gmail.com>
2024-11-18 09:50:28 +0900

    fix: env 파일 삭제 및 변수명 변경

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,8 +20,8 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
     'gatsby-plugin-image',
